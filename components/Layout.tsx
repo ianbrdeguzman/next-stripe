be586bb5623dbb1e95d786354f7a79ccf5a5ab46
ianbrdeguzman <ianbrdeguzman@gmail.com>
2022-01-23 05:38:38 -0500

    added back to home button

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,17 @@
+import Link from 'next/link';
+
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export function Layout({ children }: LayoutProps) {
   return (
-    <div className="border w-screen h-screen flex justify-center items-center text-center">
+    <div className="border w-screen h-screen flex justify-center items-center text-center flex flex-col">
+      <Link href="/">
+        <a className="hover:text-blue-900 focus:text-blue-900 hover:underline text-3xl">
+          Home
+        </a>
+      </Link>
       {children}
     </div>
   );
