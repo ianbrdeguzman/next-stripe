f41e0e09405a5acb551bdc233317d758ab0d83a2
ianbrdeguzman <ianbrdeguzman@gmail.com>
2022-01-30 05:46:06 -0500

    added cart page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,7 +9,7 @@ export function Layout({ children }: LayoutProps) {
   const { cart } = useCart();
 
   return (
-    <div className="mt-40 p-4 w-100 h-100% flex justify-center items-center text-center flex flex-col">
+    <div className="md:mt-40 p-4 w-100 h-100% flex justify-center items-center text-center flex flex-col">
       <div className="flex justify-between w-full max-w-xl">
         <Link href="/">
           <a className="hover:text-blue-900 focus:text-blue-900 hover:underline text-3xl">
--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,21 +1,43 @@
 import { useCart } from '../hooks/useCart';
 
 export default function Cart() {
-  const { cart } = useCart();
+  const { cart, addItemToCart, minusItemOnCart, removeItemFromCart } =
+    useCart();
   return (
-    <div className="w-full">
-      {cart.map((item) => (
-        <article
-          className="flex justify-between items-center mt-4 border p-4"
-          key={item.id}
-        >
-          <p className="flex-1 text-left">{item.id}</p>
-          <p>{item.qty}</p>
-          <button className="border ml-4 p-2">+1</button>
-          <button className="border ml-4 p-2">-1</button>
-          <button className="border ml-4 p-2">remove</button>
-        </article>
-      ))}
+    <div className="w-full max-w-3xl">
+      {cart.length > 0 ? (
+        cart.map((item) => (
+          <article
+            className="flex justify-between items-center mt-4 border p-4"
+            key={item.id}
+          >
+            <p className="flex-1 text-left">{item.id}</p>
+            <p>{item.qty}</p>
+            <div className="flex flex-col">
+              <button
+                onClick={() => addItemToCart({ id: item.id, qty: 1 })}
+                className="border mb-1 ml-4 p-2"
+              >
+                +1
+              </button>
+              <button
+                onClick={() => minusItemOnCart({ id: item.id, qty: 1 })}
+                className="border mt-1 ml-4 p-2"
+              >
+                -1
+              </button>
+            </div>
+            <button
+              onClick={() => removeItemFromCart(item.id)}
+              className="border ml-4 p-2"
+            >
+              remove
+            </button>
+          </article>
+        ))
+      ) : (
+        <p className="mt-40">Cart is empty.</p>
+      )}
     </div>
   );
 }
